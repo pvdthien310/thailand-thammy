3ea04cbdbae5d08edd2f915dcade34a49f814415
Thuc Tran <thuc.tran@chotot.vn>
2026-05-30 17:10:34 +0700

    feat: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Be_Vietnam_Pro } from "next/font/google";
 
-import { siteDescription, siteName, siteTitle, siteUrlObject } from "@/lib/site";
+import {
+  siteDescription,
+  siteName,
+  siteTitle,
+  siteUrlObject,
+} from "@/lib/site";
 
 import "./globals.css";
 
@@ -99,7 +104,28 @@ export default function RootLayout({
       lang="vi"
       className={`${playfair.variable} ${beVietnam.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WPSB4H4S');`,
+          }}
+        />
+      </head>
+      <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WPSB4H4S"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
